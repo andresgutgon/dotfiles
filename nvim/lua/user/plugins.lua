5d5b33e38ae6c3756754d67d845e4a7a48b8f7ec
andresgutgon <andresgutgon@gmail.com>
2022-01-19 14:23:38 +0100

    Fix treesitter failing More info in this issue: https://github.com/nvim-treesitter/nvim-treesitter/issues/2295

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -103,7 +103,11 @@ local plugins = function(use)
   }
 
   -- Treesitter
-  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    commit = '668de0951a36ef17016074f1120b6aacbe6c4515',
+    run = ":TSUpdate"
+  }
 
   -- Git related:
   use "lewis6991/gitsigns.nvim"
