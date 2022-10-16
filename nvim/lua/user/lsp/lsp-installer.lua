0e2a749b212b9d03da40d2ed5234f14e166edc47
andresgutgon <andresgutgon@gmail.com>
2022-10-16 10:32:40 +0200

    Update Nvim and fix LSP issues It's being a long time since I did some cleaning in my Nvim. So first thing I did was `brew resinstall nvim` so now I'm on latest development commit (nvim 8)
    I think before even updating I was having some errors related with LSP.

--- nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/lua/user/lsp/lsp-installer.lua
@@ -1,23 +1,49 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-	return
+  return
 end
 
--- My servers (from Fran - github:franciscoj/dotfiles)
-local installable_servers = {
-  "bashls",
-  "tailwindcss",
-  "cssls",
-  "tsserver",
-  "html",
-  "pyright",
-  "sumneko_lua",
-  "jsonls",
-  "yamlls",
-  "eslint",
-  "sorbet",
-  "html"
-}
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+  local opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
+  }
+
+  local lang_ok, lang_opts = pcall(require, "user.lsp.settings." .. server.name)
+  if lang_ok then
+    opts = vim.tbl_deep_extend("force", lang_opts, opts)
+  end
+
+  -- This setup() function is exactly the same as lspconfig's setup function.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
+end)
+
+-- install automatically all this lsp on start
+local installable_servers = {}
+
+if vim.fn.executable("node") == 1 then
+  table.insert(installable_servers, 'bashls')
+  table.insert(installable_servers, 'cssls')
+  table.insert(installable_servers, 'bashls')
+  table.insert(installable_servers, 'html')
+  table.insert(installable_servers, 'jsonls')
+  table.insert(installable_servers, 'yamlls')
+  table.insert(installable_servers, 'tailwindcss')
+  table.insert(installable_servers, 'pyright')
+  table.insert(installable_servers, 'tsserver')
+end
+
+if vim.fn.executable("ruby") == 1 then
+  table.insert(installable_servers, 'sorbet')
+end
+
+if vim.fn.executable("rustc") == 1 then
+  table.insert(installable_servers, 'rust_analyzer')
+end
+
 for _, name in pairs(installable_servers) do
   local found, server = lsp_installer.get_server(name)
   if found then
@@ -27,26 +53,3 @@ for _, name in pairs(installable_servers) do
     end
   end
 end
-
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
-
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("user.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
-
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
-
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
-
