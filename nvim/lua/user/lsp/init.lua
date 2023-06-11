4b9ebe769fccd9db8e471647b5c62ca0e9d0a47b
andresgutgon <andresgutgon@gmail.com>
2023-06-11 13:19:33 +0200

    Disable terraform because of Apple warning

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -55,7 +55,10 @@ local servers = {
   "tsserver",
   "eslint",
   "sorbet",
-  "terraform",
+  -- Apple complains about terraform-ls being dangerous.
+  -- Related with terraform binary not signed correctly or some shit
+  -- like that.
+  --[[ "terraform", ]]
   "lua_ls",
   "null-ls",
   "rust_analyzer",
