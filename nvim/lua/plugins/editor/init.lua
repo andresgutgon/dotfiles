079ceee57dc7cdd6e1b97136ff91cee13cff9b56
andresgutgon <andresgutgon@gmail.com>
2023-12-21 09:46:00 +0100

    Added telescope select ui plugin

--- nvim/lua/plugins/editor/init.lua
+++ nvim/lua/plugins/editor/init.lua
@@ -64,6 +64,19 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
   {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({})
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end
+  },
+  {
     "numToStr/Navigator.nvim",
     config = function()
       require("Navigator").setup({
