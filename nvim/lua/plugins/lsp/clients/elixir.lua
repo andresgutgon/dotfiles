b24cca4c5a80dc87f7ba3226786ba0af77a82532
andresgutgon <andresgutgon@gmail.com>
2025-09-27 13:21:20 +0200

    Support dumb monorepo with multiple Elixir packages in one git repo I have a dumb monorepo without Umbrella that has multiple packages under ./packages folder. Are all mix independent projects that I want to publish independetly. This change makes ElixirLS and Expert to compile the right mix project depending on the file opened

--- nvim/lua/plugins/lsp/clients/elixir.lua
+++ nvim/lua/plugins/lsp/clients/elixir.lua
@@ -6,8 +6,26 @@ M.setup = function(_, capabilities)
     capabilities = capabilities,
     cmd = { vim.fn.expand("~/.local/bin/expert") },
     root_dir = function(fname)
-      return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.fn.getcwd()
+      local util = require("lspconfig.util")
+      -- Find nearest mix.exs upwards
+      local mix_root = util.root_pattern("mix.exs")(fname)
+      if mix_root then
+        return mix_root
+      end
+
+      -- If opened from repo root with no mix.exs, try to detect packages
+      local git_root = util.root_pattern(".git")(fname)
+      if git_root then
+        local package_mix = util.path.join(git_root, "packages", "*", "mix.exs")
+        local matches = vim.fn.glob(package_mix, 0, 1)
+        if #matches > 0 then
+          return util.path.dirname(matches[1]) -- pick the first package
+        end
+      end
+
+      return git_root or vim.fn.getcwd()
     end,
+
     flags = {
       debounce_text_changes = 150,
     },
