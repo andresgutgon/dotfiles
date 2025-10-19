82c00fdd37251cff7753e0cf8eafe078cc17cc13
andresgutgon <andresgutgon@gmail.com>
2025-10-19 14:12:57 +0200

    feat: wip

--- nvim/lua/plugins/editor/init.lua
+++ nvim/lua/plugins/editor/init.lua
@@ -75,33 +75,6 @@ return {
     opts = { signs = false },
   },
   {
-    "steelsojka/pears.nvim",
-    config = function()
-      local R = require("pears.rule")
-      require("pears").setup(function(conf)
-        conf.preset("tag_matching")
-
-        conf.pair("'", {
-          close = "'",
-          should_expand = R.all_of(
-            -- Don't expand a quote if it comes after an alpha character
-            R.not_(R.start_of_context("[a-zA-Z]")),
-            -- Only expand when in a treesitter "string" node
-            R.child_of_node("string")
-          ),
-        })
-
-        conf.on_enter(function(pears_handle)
-          if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= -1 then
-            return vim.fn["compe#confirm"]("<CR>")
-          else
-            pears_handle()
-          end
-        end)
-      end)
-    end,
-  },
-  {
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup({
