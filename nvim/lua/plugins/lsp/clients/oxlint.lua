d2f7e72635d5b3f9e1e6e05e47fd5e33ff9d7e03
andresgutgon <andresgutgon@gmail.com>
2026-02-07 18:06:42 +0100

    Remove comment

--- nvim/lua/plugins/lsp/clients/oxlint.lua
+++ nvim/lua/plugins/lsp/clients/oxlint.lua
@@ -4,7 +4,6 @@ M.setup = function(_, capabilities)
   vim.lsp.config("oxlint", {
     cmd = { "oxlint", "--lsp" },
     capabilities = capabilities,
-    -- Only enable in projects with .oxlintrc.json
     root_markers = { ".oxlintrc.json" },
   })
   vim.lsp.enable("oxlint")
