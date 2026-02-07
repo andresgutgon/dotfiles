27f587ad07c1bf81dad93eaad160e9001b2db01c
andresgutgon <andresgutgon@gmail.com>
2026-02-07 17:06:02 +0100

    Periodic packages update

--- nvim/lua/plugins/ai/init.lua
+++ nvim/lua/plugins/ai/init.lua
@@ -6,15 +6,15 @@ return {
     config = function()
       require("copilot").setup({
         suggestion = {
-          enabled = true, -- enable/disable suggestions
+          enabled = true,      -- enable/disable suggestions
           auto_trigger = true, -- show automatically as you type
-          debounce = 75, -- lower debounce = faster suggestions
+          debounce = 75,       -- lower debounce = faster suggestions
           keymap = {
-            accept = "<Tab>", -- accept suggestion
+            accept = "<Tab>",  -- accept suggestion
             accept_word = "<C-w>",
             accept_line = "<C-e>",
-            next = "<M-]>", -- cycle forward (Alt-])
-            prev = "<M-[>", -- cycle backward (Alt-[)
+            next = "<M-]>",    -- cycle forward (Alt-])
+            prev = "<M-[>",    -- cycle backward (Alt-[)
             dismiss = "<C-c>", -- dismiss suggestion
           },
         },
