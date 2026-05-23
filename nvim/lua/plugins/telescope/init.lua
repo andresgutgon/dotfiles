bf7d4147593e59522deac5e96334066d200b365e
andresgutgon <andresgutgon@gmail.com>
2026-05-23 11:26:08 +0200

    Update telescope to latest version now that I'm in nvim 0.12.0

--- nvim/lua/plugins/telescope/init.lua
+++ nvim/lua/plugins/telescope/init.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.5",
+  branch = "master",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { -- If encountering errors, see telescope-fzf-native README for install instructions
